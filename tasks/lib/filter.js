120df586ccf66b326ca66585dc41c7733fb777a3
Bret K. Ikehara <bret.k.ikehara@gmail.com>
2014-01-28 21:01:37 -1000

    Updated pattern function name. Moved compiler functions into own file.

--- tasks/lib/filter.js
+++ tasks/lib/filter.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-    get: function (grunt, compiler, options) {
+    get: function (grunt, options) {
         return {
-            pattern: function (files, pattern) {
+            match: function (files, pattern) {
                 return files.filter(function(filepath) {
                     // Remove nonexistent files (it's up to you to filter or warn here).
                     if (pattern && !pattern.test(filepath)) {
@@ -15,47 +15,6 @@ module.exports = {
 
                     return true;
                 });
-            },
-            helper: function(filepath) {
-                var filecontent = grunt.file.read(filepath),
-                    helper;
-
-                helper = compiler.helper({
-                    filepath: filepath,
-                    helper: filecontent,
-                    opts: options.opts
-                });
-
-                return helper;
-            },
-            partial: function(filepath) {
-                var partialArray = eval(grunt.file.read(filepath, {
-                        encoding: 'utf-8'
-                    })),
-                    out = [];
-
-                partialArray.forEach(function(partialName) {
-                    var data = compiler.partial({
-                        name: partialName,
-                        opts: options.opts 
-                    });
-                    out.push(data);
-                });
-
-                return out.join('\n');
-            },
-            template: function(filepath) {
-                var filecontent = grunt.file.read(filepath),
-                    precompiled = Handlebars.precompile(Handlebars.parse(filecontent)),
-                    template;
-
-                template = compiler.template({
-                    filepath: filepath,
-                    template: precompiled,
-                    opts: options.opts
-                });
-
-                return template;
             }
         };
     }
